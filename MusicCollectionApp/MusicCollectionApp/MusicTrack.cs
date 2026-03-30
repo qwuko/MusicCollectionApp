a712e40e01d3f6785e05f5e0b080dc4771070633
Stakhaevich <verast2006@gmail.com>
2026-03-30 14:24:57 +0300

    Добавление проверки вводимых данных в классе MusicTrack

--- MusicCollectionApp/MusicCollectionApp/MusicTrack.cs
+++ MusicCollectionApp/MusicCollectionApp/MusicTrack.cs
@@ -1,4 +1,6 @@
-﻿public class MusicTrack
+﻿using System;
+
+public class MusicTrack
 {
     public string Artist { get; set; }
     public string Title { get; set; }
@@ -7,6 +9,26 @@
 
     public MusicTrack(string artist, string title, string genre, int year)
     {
+        if (string.IsNullOrWhiteSpace(artist))
+        {
+            throw new ArgumentException("Исполнитель не может быть пустым");
+        }
+
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            throw new ArgumentException("Название трека не может быть пустым");
+        }
+
+        if (string.IsNullOrWhiteSpace(genre))
+        {
+            throw new ArgumentException("Жанр трека не может быть пустым");
+        }
+
+        if (year < 1800 || year > DateTime.Now.Year)
+        {
+            throw new ArgumentException($"Год должен быть между 1800 и {DateTime.Now.Year}");
+        }
+
         Artist = artist;
         Title = title;
         Genre = genre;
